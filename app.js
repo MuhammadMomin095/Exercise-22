1158cb91aada2d5151bf0ca86cfa1ce95690900f
MuhammadMomin095 <160387789+MuhammadMomin095@users.noreply.github.com>
2024-03-06 14:49:10 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,2 @@
+var array = ['akram', 'akbar', 9, 5, 95, 'burhan', 'afzal', 6, 7, 3];
+console.log(array[9]);
---
+++ app.ts
@@ -0,0 +1,2 @@
+let array: (string | number) [] = ['akram','akbar',9,5,95,'burhan','afzal',6,7,3]
+console.log(array[9])
